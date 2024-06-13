3dc99467c5cf07ac47fa1612eaa4b1cb47a97002
rsanttos <rsanttos.tech@gmail.com>
2024-06-13 18:17:49 -0300

    creating list

--- src/screens/MotorcycleTable/index.tsx
+++ src/screens/MotorcycleTable/index.tsx
@@ -3,7 +3,119 @@ import './styles.css';
 const MotorcycleTable = () => {
   return (
     <section id='body-MotorcycleTable' className='flex'>
-      <h1>MotorcycleTable</h1>
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
+
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
+
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
+
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
+
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
+
+      <div className="flex card">
+        <span className='txt-id'>#001</span>
+
+        <div className='flex box-txt'>
+          <span className="txt-title">honda pop 1101</span>
+          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
+          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
+        </div>
+
+        <div className="flex box-icons">
+          <button 
+            className='flex material-symbols-outlined'
+            style={{color: '#FF4C51'}}
+          >delete</button>
+
+          <button className='flex material-symbols-outlined'>visibility</button>
+        </div>
+      </div>
     </section>
   );
 }
