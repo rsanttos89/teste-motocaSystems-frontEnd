3bc5fa511b254aa95c6bb76089db945f8d723d8f
rsanttos <rsanttos.tech@gmail.com>
2024-06-13 19:28:41 -0300

    mobile

--- src/screens/MotorcycleTable/index.tsx
+++ src/screens/MotorcycleTable/index.tsx
@@ -7,7 +7,11 @@ const MotorcycleTable = () => {
         <span className='txt-id'>#001</span>
 
         <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
+          <span 
+            className="txt-title">honda pop 1101 <span 
+            className='status'>em estoque</span>
+          </span>
+          
           <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
           <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
         </div>
@@ -26,83 +30,11 @@ const MotorcycleTable = () => {
         <span className='txt-id'>#001</span>
 
         <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
-          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
-          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
-        </div>
-
-        <div className="flex box-icons">
-          <button 
-            className='flex material-symbols-outlined'
-            style={{color: '#FF4C51'}}
-          >delete</button>
-
-          <button className='flex material-symbols-outlined'>visibility</button>
-        </div>
-      </div>
-
-      <div className="flex card">
-        <span className='txt-id'>#001</span>
-
-        <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
-          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
-          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
-        </div>
-
-        <div className="flex box-icons">
-          <button 
-            className='flex material-symbols-outlined'
-            style={{color: '#FF4C51'}}
-          >delete</button>
-
-          <button className='flex material-symbols-outlined'>visibility</button>
-        </div>
-      </div>
-
-      <div className="flex card">
-        <span className='txt-id'>#001</span>
-
-        <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
-          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
-          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
-        </div>
-
-        <div className="flex box-icons">
-          <button 
-            className='flex material-symbols-outlined'
-            style={{color: '#FF4C51'}}
-          >delete</button>
-
-          <button className='flex material-symbols-outlined'>visibility</button>
-        </div>
-      </div>
-
-      <div className="flex card">
-        <span className='txt-id'>#001</span>
-
-        <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
-          <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
-          <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
-        </div>
-
-        <div className="flex box-icons">
-          <button 
-            className='flex material-symbols-outlined'
-            style={{color: '#FF4C51'}}
-          >delete</button>
-
-          <button className='flex material-symbols-outlined'>visibility</button>
-        </div>
-      </div>
-
-      <div className="flex card">
-        <span className='txt-id'>#001</span>
-
-        <div className='flex box-txt'>
-          <span className="txt-title">honda pop 1101</span>
+          <span 
+            className="txt-title">honda pop 1101 <span 
+            className='status'>em estoque</span>
+          </span>
+          
           <span style={{padding: '8px 0px'}}>Valor: R$15.000,00</span>
           <span>Cor: <span style={{textTransform: 'uppercase'}}>branca</span></span>
         </div>
