fe9d1fe011e2f55aa82c417b42a1eea653b2c61f
rsanttos <rsanttos.tech@gmail.com>
2024-06-13 19:36:46 -0300

    input required

--- src/screens/MotorcycleRegistration/index.tsx
+++ src/screens/MotorcycleRegistration/index.tsx
@@ -9,19 +9,19 @@ const MotorcycleRegistration = () => {
 
       <form action="" className='flex'>
         <label htmlFor="product_code">Código</label>
-        <input type="text" id='product_code' />
+        <input type="text" id='product_code' required />
 
         <label htmlFor="product_code">Modelo da Moto</label>
-        <input type="text" id='product_code' />
+        <input type="text" id='product_code' required />
 
         <label htmlFor="product_code">Cor</label>
-        <input type="text" id='product_code' />
+        <input type="text" id='product_code' required />
 
         <label htmlFor="product_code">Valor</label>
-        <input type="text" id='product_code' />
+        <input type="text" id='product_code' required />
 
         <label htmlFor="product_code">Status</label>
-        <select id="product_code">
+        <select id="product_code" required>
           <option value="em_estoque">Em estoque</option>
           <option value="sem_estoque">Sem estoque</option>
           <option value="em_transito">Em trânsito</option>
